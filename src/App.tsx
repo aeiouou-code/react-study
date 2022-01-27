1a4608c55d4fbd162dc55c30a040d8e94bd18b63
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2022-01-27 11:14:18 +0900

    add: initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <H1>This is Title</H1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          type="text"
+          placeholder="username"
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
+    </Container>
   );
 }
-
 export default App;
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+
+  button {
+    background-color: ${(props) => props.theme.btnColor};
+  }
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
