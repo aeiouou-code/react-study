aba5708dc53a37f5bc938d449ae1c671ec4a7f0d
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2022-01-27 11:42:13 +0900

    add: Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import Router from "./Router";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-  return (
-    <Container>
-      <H1>This is Title</H1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          type="text"
-          placeholder="username"
-          onChange={onChange}
-        />
-        <button>Log in</button>
-      </form>
-    </Container>
-  );
+  return <Router />;
 }
+
 export default App;
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-
-  button {
-    background-color: ${(props) => props.theme.btnColor};
-  }
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
